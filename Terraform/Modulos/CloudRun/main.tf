d2971539410cead73cc8114b451f069863624786
jgilaberttypsa <joangs2001@gmail.com>
2025-04-14 18:58:42 +0200

    cloud run

--- Terraform/Modulos/CloudRun/main.tf
+++ Terraform/Modulos/CloudRun/main.tf
@@ -1,24 +1,32 @@
-resource "google_cloud_run_service" "default" {
-  name     = "streamlit"
+resource "google_cloud_run_service" "run-streamlit" {
+  name     = "str-image"
   location = "europe-southwest1"
 
   template {
     spec {
       containers {
-        image = "europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/streamlit@sha256:7d0d127842795e50aa493b4e735346d0d0d61680c3822ed0511730ff5d606039"
+        image = "europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/str-image@sha256:941d2822b13dfe75cbdfe1ce8c98f6e984082101e09a3511c849fcdebb0b3f3c"
 
-        # Configurar el puerto de salida
+   
         ports {
-          container_port = 8502 # Cambia el puerto aquí si es necesario
+          container_port = 8502 
         }
       }
     }
   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
+
+}
+
+resource "google_cloud_run_service_iam_policy" "public_access" {
+  service = google_cloud_run_service.run-streamlit.name
+  location = google_cloud_run_service.run-streamlit.location
+  policy_data = data.google_iam_policy.public_iam_policy.policy_data
+}
+
+data "google_iam_policy" "public_iam_policy" {
+  binding {
+    role = "roles/run.invoker"
+    members = ["allUsers"]
   }
-
-
 }
--- Terraform/Modulos/streamlit/main.tf
+++ Terraform/Modulos/streamlit/main.tf
@@ -4,14 +4,12 @@ resource "google_artifact_registry_repository" "my_repo" {
   description   = "Repository for data project"
   format        = "DOCKER"
 
-  docker_config {
-    immutable_tags = true
-  }
+
 }
 
 resource "null_resource" "docker_build" {
   provisioner "local-exec" {
-    command = "gcloud auth configure-docker europe-southwest1-docker.pkg.dev && docker build -t streamlit . && docker tag streamlit europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/streamlit:latest && docker push europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/streamlit:v1.0"
+    command = "docker build -t streamlit . && docker tag streamlit europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/str-image:latest && docker push europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/str-image:latest"
 
   }
 
