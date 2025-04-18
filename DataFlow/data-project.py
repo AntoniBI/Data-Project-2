3c28e6e933ad31ca948527cb6a63e79270dceca1
jgilaberttypsa <joangs2001@gmail.com>
2025-04-18 13:17:15 +0200

    match v1

--- DataFlow/data-project.py
+++ DataFlow/data-project.py
@@ -24,16 +24,24 @@ def decode_message(msg):
 class CalcularCoeficiente(beam.DoFn):
     def process(self, mensaje):
         import random
-        servicio, evento = mensaje
-        evento_con_coef = evento.copy()
-        evento_con_coef['coeficiente'] = random.random()
+        clave, (vehiculos, emergencias) = mensaje
+        for dict in emergencias:
+            dict['coeficiente'] = random.random()
 
-        if servicio == "Bomberos":
-            yield beam.pvalue.TaggedOutput("Bomberos", (servicio, evento))
-        elif servicio == "Policia":
-            yield beam.pvalue.TaggedOutput("Policia", (servicio, evento))
-        elif servicio == "Ambulancia":
-            yield beam.pvalue.TaggedOutput("Ambulancia", (servicio, evento))
+        if clave == "Bomberos":
+            yield beam.pvalue.TaggedOutput("Bomberos", (vehiculos, emergencias))
+        elif clave == "Policia":
+            yield beam.pvalue.TaggedOutput("Policia", (vehiculos, emergencias))
+        elif clave == "Ambulancia":
+            yield beam.pvalue.TaggedOutput("Ambulancia", (vehiculos, emergencias))
+
+class Asignacion(beam.DoFn):
+    def process(self, mensaje):
+        vehiculos, emergencias = mensaje
+        if emergencias:
+            match_evento=max(emergencias, key=lambda x: x['coeficiente'])
+            yield beam.pvalue.TaggedOutput("Match", (vehiculos, match_evento))
+        
 
             
 
@@ -46,7 +54,7 @@ def run():
             | "Decode msg 1" >> beam.Map(decode_message)
             | "Combine 1" >> beam.Map(lambda x: (x['servicio'], x))
             | "Filter Null Emergencias" >> beam.Filter(lambda x: x is not None)
-            | "Fixed Window 1" >> beam.WindowInto(beam.window.FixedWindows(10))
+            | "Fixed Window 1" >> beam.WindowInto(beam.window.SlidingWindows(60, 10))
             
         )
 
@@ -56,12 +64,14 @@ def run():
             | "Decode msg 2" >> beam.Map(decode_message)
             | "Combine 2" >> beam.Map(lambda x: (x['servicio'], x))
             | "Filter Null vehículos" >> beam.Filter(lambda x: x is not None)
-            | "Fixed Window 2" >> beam.WindowInto(beam.window.FixedWindows(10))
+            | "Fixed Window 2" >> beam.WindowInto(beam.window.SlidingWindows(60, 10))
             
         )
 
         grouped_data = (
             eventos_vehiculo, evenetos_emergencias) | "Merge PCollections" >> beam.CoGroupByKey()
+        
+        # grouped_data | "Imprimir" >> beam.Map(print)
         
         processed_data = (grouped_data
             | "Calcular Coef y partir en 3 pcollections" >> beam.ParDo(CalcularCoeficiente()).with_outputs("Bomberos", "Policia", "Ambulancia"))
@@ -70,9 +80,15 @@ def run():
         policias = processed_data.Policia
         ambulancias = processed_data.Ambulancia
 
-        bomberos | "Imprimir Policia" >> beam.Map(print)
-        policias | "Imprimir Bomberos" >> beam.Map(print)
-        ambulancias | "Imprimir Ambulancia" >> beam.Map(print)
+        asignacion = (
+            bomberos
+            | "Asignación de Bomberos" >> beam.ParDo(Asignacion()).with_outputs("Match", "No Match")
+            )
+
+        Match = asignacion.Match
+
+        Match | "Imprimir Match" >> beam.Map(print)
+        
 
        
 
