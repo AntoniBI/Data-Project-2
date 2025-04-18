08a0e98b2b396808e887bbb080a4e889ddd6d83a
AntoniBI <mollantoni02@gmail.com>
2025-04-18 13:17:14 +0200

    update grafana

--- Terraform/Modulos/CloudRunApi/main.tf
+++ Terraform/Modulos/CloudRunApi/main.tf
@@ -1,15 +1,15 @@
-resource "google_cloud_run_service" "run-streamlit" {
+resource "google_cloud_run_service" "run-API" {
   name     = "str-image"
   location = "europe-southwest1"
 
   template {
     spec {
       containers {
-        image = "europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo/str-image@sha256:941d2822b13dfe75cbdfe1ce8c98f6e984082101e09a3511c849fcdebb0b3f3c"
+        image = "europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-repo-1/str-image@sha256:2119e36b36e07dd504cf6a57ed3a2c7ed23cbc28011631a4b6ce8c06fa02d9ce"
 
    
         ports {
-          container_port = 8502 
+          container_port = 8082
         }
       }
     }
@@ -19,8 +19,8 @@ resource "google_cloud_run_service" "run-streamlit" {
 }
 
 resource "google_cloud_run_service_iam_policy" "public_access" {
-  service = google_cloud_run_service.run-streamlit.name
-  location = google_cloud_run_service.run-streamlit.location
+  service = google_cloud_run_service.run-API.name
+  location = google_cloud_run_service.run-API.location
   policy_data = data.google_iam_policy.public_iam_policy.policy_data
 }
 
