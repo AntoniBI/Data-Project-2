1eadaa8bb45e442701c61339f2636e30bec60ab6
AntoniBI <mollantoni02@gmail.com>
2025-04-20 21:44:30 +0200

    Cloud Jobs

---
+++ Terraform/Modulos/CloudJobGeneradorApp/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  description = "splendid-strand-452918-e6"
+}
+
+variable "region" {
+  description = "Región donde se desplegarán los recursos"
+  default     = "europe-southwest1"
+}
