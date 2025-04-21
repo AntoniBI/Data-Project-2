ae246e47c068331a1ee71bf62d79cb8f5545e563
jgilaberttypsa <joangs2001@gmail.com>
2025-04-21 22:52:47 +0200

    all_matches

--- DataFlow/data-project.py
+++ DataFlow/data-project.py
@@ -43,27 +43,39 @@ class CalcularCoeficiente(beam.DoFn):
 class Asignacion(beam.DoFn):
     def process(self, mensaje):
         vehiculos, emergencias = mensaje
+        id_asignados = set()
         if emergencias and vehiculos:
             numero_posiciones=len(emergencias[0]["coeficientes"])
             for emergencia in emergencias:
                 for i in range(numero_posiciones):
                     match_evento = max(emergencias, key=lambda x: x["coeficientes"][i])
                     yield beam.pvalue.TaggedOutput("Match", (vehiculos[i], match_evento))
-        
-
-            
+                    id_asignados.add(id(match_evento))
+                
+                if id(emergencia) not in id_asignados:
+                    yield beam.pvalue.TaggedOutput("No Match", (emergencia))
 
 def run():
     with beam.Pipeline(options=PipelineOptions(streaming=True, save_main_session=True)) as p:
         
-        evenetos_emergencias = (
+        emergencia_nueva = (
             p 
             | "ReadFromPubSubEvent1" >> beam.io.ReadFromPubSub(subscription=f'projects/splendid-strand-452918-e6/subscriptions/emergencias_events-sub')
+            
+        )
+
+        no_match= (
+            p
+            | "ReadFromPubSubEvent3" >> beam.io.ReadFromPubSub(subscription=f'projects/splendid-strand-452918-e6/subscriptions/no_match-sub')
+        )
+
+        evenetos_emergencias = (
+            (emergencia_nueva, no_match)
+            | "Flatten Emergencias" >> beam.Flatten()
             | "Decode msg 1" >> beam.Map(decode_message)
             | "Combine 1" >> beam.Map(lambda x: (x['servicio'], x))
             | "Filter Null Emergencias" >> beam.Filter(lambda x: x is not None)
             | "Fixed Window 1" >> beam.WindowInto(beam.window.SlidingWindows(60, 10))
-            
         )
 
         eventos_vehiculo = ( 
@@ -79,7 +91,6 @@ def run():
         grouped_data = (
             eventos_vehiculo, evenetos_emergencias) | "Merge PCollections" >> beam.CoGroupByKey()
         
-        # grouped_data | "Imprimir" >> beam.Map(print)
         
         processed_data = (grouped_data
             | "Calcular Coef y partir en 3 pcollections" >> beam.ParDo(CalcularCoeficiente()).with_outputs("Bomberos", "Policia", "Ambulancia"))
@@ -88,16 +99,30 @@ def run():
         policias = processed_data.Policia
         ambulancias = processed_data.Ambulancia
 
-        asignacion = (
+        asignacion_bomb = (
             bomberos
             | "Asignación de Bomberos" >> beam.ParDo(Asignacion()).with_outputs("Match", "No Match")
             )
+        asignacion_pol = (
+            policias
+            | "Asignación de Policias" >> beam.ParDo(Asignacion()).with_outputs("Match", "No Match")
+            )
+        asignacion_amb = (
+            ambulancias
+            | "Asignación de Ambulancias" >> beam.ParDo(Asignacion()).with_outputs("Match", "No Match")
+            )
 
-        Match = asignacion.Match
+        all_no_matches = (
+                (asignacion_bomb.NoMatch, asignacion_pol.NoMatch, asignacion_amb.NoMatch)
+                | "Flatten No Matches" >> beam.Flatten()
+            )
 
-        Match | "Imprimir Match" >> beam.Map(print)
-        
+        all_no_matches | "Enviar a Topic de Reintento" >> beam.io.WriteToPubSub(topic="")
 
-       
+        all_matches = (
+                (asignacion_bomb.Match, asignacion_pol.Match, asignacion_amb.Match)
+                | "Flatten Matches" >> beam.Flatten()
+            )
+        # Escribir en BigQuery los all matches. Pensar que hacer con los no matches.
 
 run()
