cbf21aef7ce22aca63556dec8fe399e326af85b4
AntoniBI <mollantoni02@gmail.com>
2025-04-29 16:04:43 +0200

    Update Unificar Terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -5,4 +5,28 @@ terraform {
   }
 }
 
+module "artifact_registry" {
+  source     = "./Modulos/ArtifactRegistry"
+  project_id = "splendid-strand-452918-e6"
+  region     = "europe-southwest1"
+}
 
+module "bigquery" {
+  source     = "./Modulos/Bigquery"
+  project_id = "splendid-strand-452918-e6"
+  region     = "europe-southwest1"
+}
+
+module "pubsub" {
+  source     = "./Modulos/pubsub"
+  project_id = "splendid-strand-452918-e6"
+  region     = "europe-southwest1"
+}
+
+# module "cloud_run_service" {
+#   source       = "./Modulos/cloud_run_service"
+#   project_id   = "splendid-strand-452918-e6"
+#   region       = "europe-southwest1"
+#   service_name = "str-service"
+#   image_url    = "europe-southwest1-docker.pkg.dev/splendid-strand-452918-e6/data-project-2/str-api:latest"
+# }
